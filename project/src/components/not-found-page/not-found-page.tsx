a33847f6ca4ff836d6d73c5f7f418c238aa38450
Stanislav Vitkovskiy <Stasvitkovskiy700@gmail.com>
2021-10-03 21:26:30 +0300

    Created new React-components

---
+++ project/src/components/not-found-page/not-found-page.tsx
@@ -0,0 +1,21 @@
+function notFoundPage(): JSX.Element {
+  return (
+    <div style={{
+      position: 'absolute',
+      top: '0',
+      bottom: '0',
+      right: '0',
+      left: '0',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center'}}
+    >
+      <p style={{fontSize: '50px', margin: '0'}}>404 Not Found</p>
+      <p style={{fontSize: '20px', color: 'blue'}}>На главную</p>
+    </div>
+  );
+
+}
+
+export default notFoundPage;
