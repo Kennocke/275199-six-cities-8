c2d652319679054c7bb645d5ef1f87b686241d30
Stanislav Vitkovskiy <Stasvitkovskiy700@gmail.com>
2021-10-14 22:12:03 +0300

    Added routing

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,11 @@
 import MainPage from '../main-page/main-page';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import LoginPage from '../login-page/login-page';
+import FavoritesPage from '../favorites-page/favorites-page';
+import OfferPage from '../offer-page/offer-page';
+import NotFoundPage from '../not-found-page/not-found-page';
+import PrivateRoute from '../private-route/private-route';
+import {AuthorizationStatus} from '../../const';
 
 type AppProps = {
   offersCount: number;
@@ -6,10 +13,32 @@ type AppProps = {
 
 function App(props: AppProps): JSX.Element {
   return (
-    <MainPage
-      offersCount = {props.offersCount}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/'>
+          <MainPage
+            offersCount = {props.offersCount}
+          />
+        </Route>
+        <Route exact path='/login'>
+          <LoginPage/>
+        </Route>
+        <PrivateRoute
+          exact
+          path='/favorites'
+          render={() => <FavoritesPage/>}
+          authorizationStatus={AuthorizationStatus.Auth}
+        />
+        <Route exact path='/offer/:id'>
+          <OfferPage/>
+        </Route>
+        <Route>
+          <NotFoundPage/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
